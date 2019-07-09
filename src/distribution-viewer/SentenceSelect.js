5e8ed025ebd7debb3d55663e1e1834a1fb3e91a7
Quentin Roy <quentin@quentinroy.fr>
2019-07-09 11:26:39 -0400

    optimize SentenceSelect

--- src/distribution-viewer/SentenceSelect.js
+++ src/distribution-viewer/SentenceSelect.js
@@ -1,27 +1,34 @@
-import React from "react";
+import React, { memo } from "react";
 import PropTypes from "prop-types";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import useMultiRef from "../utils/useMultiRef";
 
-export default function SentenceSelect({
-  corpus,
-  selectedIndex,
-  setSelectedIndex
-}) {
+// The list is long. Extracting this in its own component so we can optimize it.
+const SentenceItem = memo(({ onClick, sentence, selected }) => (
+  <ListItem button onClick={onClick} selected={selected}>
+    <ListItemText primary={sentence} />
+  </ListItem>
+));
+
+SentenceItem.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  sentence: PropTypes.string.isRequired,
+  selected: PropTypes.bool.isRequired
+};
+
+const SentenceSelect = memo(({ corpus, selectedIndex, setSelectedIndex }) => {
   const callbacks = useMultiRef(corpus.length, i => () => setSelectedIndex(i));
 
   const items = corpus.map((sentence, i) => (
-    <ListItem
+    <SentenceItem
       // eslint-disable-next-line react/no-array-index-key
       key={i}
-      button
       onClick={callbacks[i]}
       selected={selectedIndex === i}
-    >
-      <ListItemText primary={sentence} />
-    </ListItem>
+      sentence={sentence}
+    />
   ));
 
   return (
@@ -29,10 +36,12 @@ export default function SentenceSelect({
       {items}
     </List>
   );
-}
+});
 
 SentenceSelect.propTypes = {
   corpus: PropTypes.arrayOf(PropTypes.string).isRequired,
   selectedIndex: PropTypes.number.isRequired,
   setSelectedIndex: PropTypes.func.isRequired
 };
+
+export default SentenceSelect;

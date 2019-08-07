4cb56a5e13fc237e305970926639f9677c5ddb34
Quentin Roy <quentin@quentinroy.fr>
2019-08-07 16:40:39 -0400

    typo

--- src/experiment/useConfiguration.js
+++ src/experiment/useConfiguration.js
@@ -67,7 +67,7 @@ const generateTasks = corpus => {
   if (numberOfPracticeTasks > 0) {
     tasks.push({
       task: "InformationScreen",
-      content: `The ${numberOfPracticeTasks} following tasks are practice tasks`,
+      content: `The ${numberOfPracticeTasks} following task(s) are practice tasks`,
       shortcutEnabled: true,
       key: `${tasks.length}`
     });

ba24ccf618b102a639fd7436c43204e461ee4c9b
Quentin Roy <quentin@quentinroy.fr>
2019-08-14 16:20:36 -0400

    remove unused hook

--- src/experiment/hooks/useHasWindowFocus.js
+++
@@ -1,9 +0,0 @@
-import { useState, useEffect } from "react";
-
-const useHasWindowFocus = () => {
-  const [hasWindowFocus, setHasWindowFocus] = useState(document.hasFocus());
-
-  return hasWindowFocus;
-};
-
-export default useHasWindowFocus;

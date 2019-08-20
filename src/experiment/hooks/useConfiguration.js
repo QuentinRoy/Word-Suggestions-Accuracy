e036e0cc5131f1b3e4095eb42f2d00121325af76
Quentin Roy <quentin@quentinroy.fr>
2019-08-20 12:28:35 -0400

    fix config

--- src/experiment/hooks/useConfiguration.js
+++ src/experiment/hooks/useConfiguration.js
@@ -98,75 +98,75 @@ const generateTasks = corpus => {
 
   const tasks = [];
 
-  // tasks.push(Task(TaskTypes.consentForm, { key: `consent-${tasks.length}` }));
+  tasks.push(Task(TaskTypes.consentForm, { key: `consent-${tasks.length}` }));
 
-  // tasks.push(UploadLogTask(`upload-${tasks.length}`, true));
+  tasks.push(UploadLogTask(`upload-${tasks.length}`, true));
 
-  // tasks.push(Task(TaskTypes.startup, { key: `startup-${tasks.length}` }));
+  tasks.push(Task(TaskTypes.startup, { key: `startup-${tasks.length}` }));
 
-  // tasks.push(UploadLogTask(`upload-${tasks.length}`, true));
+  tasks.push(UploadLogTask(`upload-${tasks.length}`, true));
 
-  // tasks.push(
-  //   Task(TaskTypes.tutorial, {
-  //     key: `tuto-${tasks.length}`,
-  //     isPractice: true
-  //   })
-  // );
+  tasks.push(
+    Task(TaskTypes.tutorial, {
+      key: `tuto-${tasks.length}`,
+      isPractice: true
+    })
+  );
 
-  // tasks.push(UploadLogTask(`upload-${tasks.length}`, true));
+  tasks.push(UploadLogTask(`upload-${tasks.length}`, true));
 
-  // // Insert practice tasks.
-  // if (numberOfPracticeTasks > 0) {
-  //   tasks.push(
-  //     Task(TaskTypes.informationScreen, {
-  //       content: "Continue with the practice tasks",
-  //       shortcutEnabled: true,
-  //       key: `info-${tasks.length}`
-  //     })
-  //   );
-  //   pickCorpusEntries(numberOfPracticeTasks).forEach(props => {
-  //     tasks.push(TypingTask(`practice-${tasks.length}`, true, props));
-  //   });
-  //   tasks.push(
-  //     Task(TaskTypes.informationScreen, {
-  //       content:
-  //         "Practice is over. You may take a break. The real experiment begins immediately after this screen!",
-  //       key: `info-${tasks.length}`
-  //     })
-  //   );
-  // }
+  // Insert practice tasks.
+  if (numberOfPracticeTasks > 0) {
+    tasks.push(
+      Task(TaskTypes.informationScreen, {
+        content: "Continue with the practice tasks",
+        shortcutEnabled: true,
+        key: `info-${tasks.length}`
+      })
+    );
+    pickCorpusEntries(numberOfPracticeTasks).forEach(props => {
+      tasks.push(TypingTask(`practice-${tasks.length}`, true, props));
+    });
+    tasks.push(
+      Task(TaskTypes.informationScreen, {
+        content:
+          "Practice is over. You may take a break. The real experiment begins immediately after this screen!",
+        key: `info-${tasks.length}`
+      })
+    );
+  }
 
-  // tasks.push(UploadLogTask(`upload-${tasks.length}`, true));
+  tasks.push(UploadLogTask(`upload-${tasks.length}`, true));
 
-  // // Insert measured tasks.
-  // pickCorpusEntries(numberOfTypingTasks).forEach(props => {
-  //   tasks.push(TypingTask(`trial-${tasks.length}`, true, props));
-  // });
+  // Insert measured tasks.
+  pickCorpusEntries(numberOfTypingTasks).forEach(props => {
+    tasks.push(TypingTask(`trial-${tasks.length}`, true, props));
+  });
 
-  // tasks.push(UploadLogTask(`upload-${tasks.length}`, true));
+  tasks.push(UploadLogTask(`upload-${tasks.length}`, true));
 
-  // tasks.push(Task(TaskTypes.endQuestionnaire, { key: `${tasks.length}` }));
-  // tasks.push(Task(TaskTypes.injectEnd, { key: `${tasks.length}` }));
+  tasks.push(Task(TaskTypes.endQuestionnaire, { key: `${tasks.length}` }));
+  tasks.push(Task(TaskTypes.injectEnd, { key: `${tasks.length}` }));
 
-  // tasks.push(UploadLogTask(`upload-${tasks.length}`, true));
+  tasks.push(UploadLogTask(`upload-${tasks.length}`, true));
 
-  // tasks.push(
-  //   Task(TaskTypes.informationScreen, {
-  //     content: `To finish, please complete a few additional typing tasks without impairment or suggestions.`,
-  //     shortcutEnabled: true,
-  //     key: `info-${tasks.length}`
-  //   })
-  // );
+  tasks.push(
+    Task(TaskTypes.informationScreen, {
+      content: `To finish, please complete a few additional typing tasks without impairment or suggestions.`,
+      shortcutEnabled: true,
+      key: `info-${tasks.length}`
+    })
+  );
 
-  // pickCorpusEntries(numberOfTypingSpeedTasks).forEach(props => {
-  //   tasks.push(
-  //     TypingTask(`typing-${tasks.length}`, true, {
-  //       ...props,
-  //       suggestionsType: SuggestionTypes.none,
-  //       keyStrokeDelay: 0
-  //     })
-  //   );
-  // });
+  pickCorpusEntries(numberOfTypingSpeedTasks).forEach(props => {
+    tasks.push(
+      TypingTask(`typing-${tasks.length}`, true, {
+        ...props,
+        suggestionsType: SuggestionTypes.none,
+        keyStrokeDelay: 0
+      })
+    );
+  });
 
   tasks.push(
     Task(TaskTypes.finalFeedbacks, { key: `feedbacks-${tasks.length}` })

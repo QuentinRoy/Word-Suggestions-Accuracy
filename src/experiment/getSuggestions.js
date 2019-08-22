8010e305850cc29d25b2867f1a6d45b8c2291655
Quentin Roy <quentin@quentinroy.fr>
2019-08-22 19:16:50 -0400

    change the criterion that removes suggestion too close from the target

--- src/experiment/getSuggestions.js
+++ src/experiment/getSuggestions.js
@@ -4,7 +4,7 @@ import {
   totalMatchedChars,
   totalMatchedCharsFromStart
 } from "../utils/strings";
-import { getCurrentInputWord } from "./input";
+import { getCurrentInputWord, getRksImprovement } from "./input";
 
 /** Compute a suggestion score based on an input word as specified in
  * https://android.googlesource.com/platform/packages/inputmethods/LatinIME/+/jb-release/native/jni/src/correction.cpp#1098
@@ -38,13 +38,6 @@ const suggestionScore = (suggestionFScore, suggestion, inputWord) => {
   return score / maxScore;
 };
 
-function isCloseFromTargetWord(word, targetWord) {
-  return (
-    targetWord.startsWith(word.slice(0, -2)) ||
-    word.startsWith(targetWord.slice(0, -2))
-  );
-}
-
 function computeSuggestions(
   inputWord,
   targetWordSKS,
@@ -72,23 +65,36 @@ function computeSuggestions(
     insertEject(topWords, { word, score }, wordEntryScoreGetter);
   };
 
+  const lowerCaseTargetWord =
+    targetWord != null ? targetWord.toLowerCase() : null;
+
+  const lowerCaseInputWord = inputWord.toLowerCase();
+
   if (
-    targetWord != null &&
+    lowerCaseTargetWord != null &&
     targetWordSKS >=
-      targetWord.length - totalMatchedChars(targetWord, inputWord)
+      getRksImprovement(
+        lowerCaseInputWord,
+        lowerCaseTargetWord,
+        lowerCaseTargetWord
+      ) &&
+    (canReplaceLetters || lowerCaseTargetWord.startsWith(lowerCaseInputWord))
   ) {
     insertTopWord(targetWord, Number.POSITIVE_INFINITY);
   }
 
-  const lowerCaseTargetWord =
-    targetWord != null ? targetWord.toLowerCase() : null;
-  const lowerCaseInputWord = inputWord.toLowerCase();
   for (let i = 0; i < dictionary.length; i += 1) {
     const { word, f: frequencyScore } = dictionary[i];
     const lowercaseWord = word.toLowerCase();
+
     if (
+      lowercaseWord !== lowerCaseTargetWord &&
       (lowerCaseTargetWord == null ||
-        !isCloseFromTargetWord(lowercaseWord, lowerCaseTargetWord)) &&
+        getRksImprovement(
+          lowerCaseInputWord,
+          lowercaseWord,
+          lowerCaseTargetWord
+        ) <= 0) &&
       (canReplaceLetters || lowercaseWord.startsWith(lowerCaseInputWord))
     ) {
       const score = suggestionScore(

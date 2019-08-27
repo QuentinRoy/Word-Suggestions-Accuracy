4dfac0870d8fb2e41362780e1ecfcaf4182ba06c
Quentin Roy <quentin@quentinroy.fr>
2019-08-26 20:34:39 -0400

    wrap the end page in a paper component

--- src/experiment/components/EndExperiment.js
+++ src/experiment/components/EndExperiment.js
@@ -1,17 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Paper } from "@material-ui/core";
 import styles from "./styles/EndExperiment.module.css";
 
 const EndExperiment = ({ confirmationCode }) => {
   return (
-    <div className={styles.main}>
-      <div className={styles.content}>
-        <h1>Thank you!</h1>
-        Here is your confirmation code. Make sure you properly copy it in the
-        corresponding text box on Amazon Mechanical Turk:
-        <p className={styles.code}>{confirmationCode}</p>
-      </div>
-    </div>
+    <Paper className={styles.main}>
+      <h1>Thank you!</h1>
+      Here is your confirmation code. Make sure you properly copy it in the
+      corresponding text box on Amazon Mechanical Turk:
+      <p className={styles.code}>{confirmationCode}</p>
+    </Paper>
   );
 };
 

ea1528438bbbfc1222a98c5f2d6b082bbbefa3a7
Quentin Roy <quentin@quentinroy.fr>
2019-09-11 20:24:25 -0400

    some comments in the qualif script

--- scripts/associate-participant-qualification.js
+++ scripts/associate-participant-qualification.js
@@ -15,6 +15,8 @@ AWS.config.update({ region, endpoint: endPoint });
 
 const mturk = new AWS.MTurk({ apiVersion: "2017-01-17" });
 
+// Consumes a the id of a worker, and (asynchronously) set up the qualif to
+// this worker.
 async function associateQualification(workerId) {
   const resp = await mturk
     .associateQualificationWithWorker({
@@ -27,12 +29,7 @@ async function associateQualification(workerId) {
   log.debug(resp);
 }
 
-async function getWorkerIdFromLog(logFilePath) {
-  const contentBuffer = await fs.readFile(logFilePath);
-  const content = JSON.parse(contentBuffer.toString());
-  return content.participant;
-}
-
+// Requests all workers with the qualification.
 async function getAllQualificationWorkers({ nextToken, acc = new Set() } = {}) {
   const resp = await mturk
     .listWorkersWithQualificationType({
@@ -51,6 +48,12 @@ async function getAllQualificationWorkers({ nextToken, acc = new Set() } = {}) {
   return getAllQualificationWorkers({ nextToken: resp.NextToken, acc });
 }
 
+async function getWorkerIdFromLog(logFilePath) {
+  const contentBuffer = await fs.readFile(logFilePath);
+  const content = JSON.parse(contentBuffer.toString());
+  return content.participant;
+}
+
 const Actions = {
   notWorker: Symbol("not a worker"),
   hasQualif: Symbol("already has the qualification"),
@@ -58,21 +61,29 @@ const Actions = {
 };
 
 async function main() {
+  // Get every workers who already have the qualification.
   const workersWithQualif = await getAllQualificationWorkers();
+  // Look for every log files here.
   const logFiles = await fs.readdir(logDirPath);
+  // For every json file, look for the worker id, and assign that worker the
+  // qualification if it does not already have it.
   const results = await Promise.all(
     logFiles
       .filter(fileName => path.extname(fileName).toLowerCase() === ".json")
       .map(async fileName => {
+        // Get the worker Id from the log file.
         const workerId = await getWorkerIdFromLog(
           path.join(logDirPath, fileName)
         );
+        // Ignores participant ids that does not look like a worker id.
         if (!/^A[A-Z0-9]+$/.test(workerId)) {
           return Actions.notWorker;
         }
+        // Ignores every worker who already has the qualification.
         if (workersWithQualif.has(workerId)) {
           return Actions.hasQualif;
         }
+        // Assign that worker its qualification.
         await associateQualification(workerId);
         log.info("Qualification associated with", workerId);
         return Actions.associated;

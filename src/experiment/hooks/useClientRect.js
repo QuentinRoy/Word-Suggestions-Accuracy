05f0f30a6250c6ee01807af232576a2f3caaa7f9
Quentin Roy <quentin@quentinroy.fr>
2020-01-23 17:15:17 -0500

    much simplifies useClientRect

--- src/experiment/hooks/useClientRect.js
+++ src/experiment/hooks/useClientRect.js
@@ -1,40 +1,12 @@
-import { useState, useCallback, useRef, useEffect } from "react";
+import { useState, useMemo } from "react";
 
 export default function useClientRect() {
-  const [rect, setRect] = useState(null);
-  const innerRef = useRef(null);
+  const [node, setNode] = useState(null);
 
-  const onChange = () => {
-    if (innerRef.current == null) {
-      setRect(null);
-    } else {
-      setRect(innerRef.current.getBoundingClientRect());
-    }
-  };
-
-  const ref = useCallback(
-    newNode => {
-      innerRef.current = newNode;
-      onChange();
-    },
-    // onChange is not in the dependency list because it does not matter if the
-    // last created function is used, or if it is one from before. onChange only
-    // depends on stuff that never changes (set rect and innerRef).
-    []
+  const rect = useMemo(
+    () => (node == null ? null : node.getBoundingClientRect()),
+    [node]
   );
 
-  useEffect(
-    () => {
-      window.addEventListener("resize", onChange);
-      return () => {
-        window.removeEventListener("resize", onChange);
-      };
-    },
-    // Again, onChange is not in the dependencies because it does not matter.
-    // What is important is that the onChange function being removed as a
-    // listener is the one that was added, and that is the case.
-    []
-  );
-
-  return [rect, ref];
+  return [rect, setNode];
 }

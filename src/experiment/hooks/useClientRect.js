eedfb6390160abdede95fdc8a673df860e1be26b
Quentin Roy <quentin@quentinroy.fr>
2020-01-23 17:00:05 -0500

    comments

--- src/experiment/hooks/useClientRect.js
+++ src/experiment/hooks/useClientRect.js
@@ -12,17 +12,29 @@ export default function useClientRect() {
     }
   };
 
-  const ref = useCallback(newNode => {
-    innerRef.current = newNode;
-    onChange();
-  }, []);
+  const ref = useCallback(
+    newNode => {
+      innerRef.current = newNode;
+      onChange();
+    },
+    // onChange is not in the dependency list because it does not matter if the
+    // last created function is used, or if it is one from before. onChange only
+    // depends on stuff that never changes (set rect and innerRef).
+    []
+  );
 
-  useEffect(() => {
-    window.addEventListener("resize", onChange);
-    return () => {
-      window.removeEventListener("resize", onChange);
-    };
-  }, []);
+  useEffect(
+    () => {
+      window.addEventListener("resize", onChange);
+      return () => {
+        window.removeEventListener("resize", onChange);
+      };
+    },
+    // Again, onChange is not in the dependencies because it does not matter.
+    // What is important is that the onChange function being removed as a
+    // listener is the one that was added, and that is the case.
+    []
+  );
 
   return [rect, ref];
 }

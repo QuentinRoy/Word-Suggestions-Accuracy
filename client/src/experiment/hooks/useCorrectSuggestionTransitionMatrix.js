b54eb8d29dd1b610c65f7df34137476bd698dff9
Quentin Roy <quentin@quentinroy.fr>
2020-02-21 18:05:36 -0500

    remove a useless file

--- client/src/experiment/hooks/useCorrectSuggestionTransitionMatrix.js
+++
@@ -1,8 +0,0 @@
-import TransitionMatrix from "../wordSuggestions/CorrectSuggestionTransitionMatrix";
-import useAsync from "../../utils/useAsync";
-import { transitionMatrixPath } from "../../utils/constants";
-
-const useCorrectSuggestionTransitionMatrix = (url = transitionMatrixPath) =>
-  useAsync(() => TransitionMatrix.fetch(url), [url]);
-
-export default useCorrectSuggestionTransitionMatrix;

e50d2e610138ac14650c1414008ec4797d6c3de3
Quentin Roy <quentin@quentinroy.fr>
2020-03-12 18:41:17 -0400

    fix typing speed result task hack

--- client/src/typing-test/ResultsTask.js
+++ client/src/typing-test/ResultsTask.js
@@ -1,39 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import PropTypes from "prop-types";
+import { withRawConfiguration } from "@hcikit/workflow";
 import style from "./ResultsTask.module.css";
 import { TaskTypes } from "../utils/constants";
 import TaskPaper from "../experiment/components/TaskPaper";
 
-const getTypingTasksFromState = () => {
-  const state = JSON.parse(localStorage.getItem("state"));
-  return state.Configuration.children.filter(
+function ResultsTask({ participant, configuration }) {
+  const typingTasks = configuration.children.filter(
     t => t.task === TaskTypes.typingSpeedTask
   );
-};
-
-export default function ResultsTask({ participant }) {
-  const [typingTasks, setTypingTasks] = useState(getTypingTasksFromState());
-  const typingResults = typingTasks.filter(t => t.trial != null);
-  const typingTaskSpeeds = typingResults.map(t => {
+  const typingTaskSpeeds = typingTasks.map(t => {
     const firstCharEvent = t.events.find(e => e.input !== "");
     const completionEvent = t.events.find(e => e.isTargetCompleted);
     return (
-      t.phrase.length /
-      ((new Date(completionEvent.time) - new Date(firstCharEvent.time)) / 1000)
+      t.phrase.length / ((completionEvent.time - firstCharEvent.time) / 1000)
     );
   });
   const avgSpeed =
     typingTaskSpeeds.reduce((a, b) => a + b, 0) / typingTaskSpeeds.length;
-
-  const willRefresh = typingTasks.length !== typingResults.length;
-
-  useEffect(() => {
-    if (willRefresh) {
-      setTimeout(() => {
-        setTypingTasks(getTypingTasksFromState());
-      }, 100);
-    }
-  });
 
   return (
     <TaskPaper>
@@ -44,11 +28,27 @@ export default function ResultsTask({ participant }) {
         <li>characters per second: {avgSpeed}</li>
         <li>words per minute: {(avgSpeed * 60) / 5}</li>
       </ul>
-      {willRefresh && <p>Refreshing...</p>}
     </TaskPaper>
   );
 }
 
 ResultsTask.propTypes = {
-  participant: PropTypes.string.isRequired
+  participant: PropTypes.string.isRequired,
+  configuration: PropTypes.shape({
+    children: PropTypes.arrayOf(
+      PropTypes.shape({
+        task: PropTypes.string.isRequired,
+        phrase: PropTypes.string,
+        events: PropTypes.arrayOf(
+          PropTypes.shape({
+            time: PropTypes.instanceOf(Date).isRequired,
+            input: PropTypes.string.isRequired,
+            isTargetCompleted: PropTypes.bool.isRequired
+          })
+        )
+      })
+    ).isRequired
+  }).isRequired
 };
+
+export default withRawConfiguration(ResultsTask);

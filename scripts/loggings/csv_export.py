53c7a677c98ca73d728a0daf1248f1ac37a3d85b
Quentin Roy <hi@quentinroy.fr>
2022-07-06 16:25:36 +0200

    feat(client-scripts): export target word with events

--- scripts/loggings/csv_export.py
+++ scripts/loggings/csv_export.py
@@ -16,6 +16,7 @@ def csv_export(
     iterator=null_iterator,
     participant_registry_path=None,
     run_registry_path=None,
+    log_progress=lambda x: None,
 ):
 
     p_registry = (
@@ -54,26 +55,27 @@ def csv_export(
         new_row["run_id"] = r_registry.get(row["run_id"])
         return new_row
 
+    def map_row(row):
+        row = encrypt_run_id(row)
+        row = encrypt_participant(row)
+        return row
+
     with open(output_file_path, "w") as log_file:
         writer = csv.DictWriter(log_file, header, quoting=csv.QUOTE_NONNUMERIC)
         writer.writeheader()
-
+        row_num = 0
+        log_progress(row_num)
         for p_file_name in os.listdir(json_logs_dir):
             if not os.path.splitext(p_file_name)[1].lower() == ".json":
                 continue
             file_path = os.path.join(json_logs_dir, p_file_name)
             with open(file_path, "r") as participant_file:
                 log = json.loads(participant_file.read())
-
-                def map_row(row):
-                    row = encrypt_run_id(row)
-                    row = encrypt_participant(row)
-                    return row
-
                 for record in iterator(log):
-                    writer.writerows(
-                        map(map_row, reader(record, file_name=p_file_name))
-                    )
+                    for row in reader(record, file_name=p_file_name):
+                        row_num += 1
+                        writer.writerow(map_row(row))
+                        log_progress(row_num)
 
     if p_registry is not None:
         p_registry.close()
--- scripts/loggings/export_events.py
+++ scripts/loggings/export_events.py
@@ -8,8 +8,8 @@ from utils import copy_rename
 
 IS_ANONYMOUS = True
 
-this_dir = os.path.dirname(os.path.abspath(__file__))
-json_logs_dir = os.path.join(this_dir, "../../participants-logs/")
+log_dir = "/Users/quentinroy/Documents/Projects/Word Suggestions/data 2021-03-15"
+json_logs_dir = os.path.join(log_dir, "first-experiments")
 output_file_path = os.path.abspath(os.path.join(json_logs_dir, "events.csv"))
 p_registry_path = os.path.abspath(os.path.join(json_logs_dir, "p_registry.csv"))
 r_registry_path = os.path.abspath(os.path.join(json_logs_dir, "r_registry.csv"))
@@ -49,6 +49,37 @@ event_columns = {
 trial_columns = {"sentence": "sentence"}
 
 
+def get_correct_input(input, sentence):
+    result = ""
+    for i in range(0, min(len(input), len(sentence))):
+        if input[i] == sentence[i]:
+            result += input[i]
+        else:
+            break
+    return result
+
+
+def get_sentence_words(sentence):
+    return list(filter(lambda w: len(w) > 0, sentence.split(" ")))
+
+
+def get_target_word_idx(event, previous_event, sentence, sentence_words):
+    if previous_event is None:
+        return 0
+    correct_input_before = (
+        previous_event["input"]
+        if previous_event["isError"]
+        else get_correct_input(previous_event["input"], sentence)
+    )
+    input_length = len(correct_input_before)
+    sentence_size = 0
+    for word_idx, word in enumerate(sentence_words):
+        sentence_size += len(word) + 1  # +1 for space.
+        if sentence_size > input_length:
+            return word_idx
+    return None
+
+
 def iter_events(task, file_name, **kwargs):
     if not "start" in task:
         return
@@ -59,14 +90,33 @@ def iter_events(task, file_name, **kwargs):
     )
     copy_rename(task, base, log_columns)
     copy_rename(task["trial"], base, trial_columns)
+    sentence = task["trial"]["sentence"]
+    sentence_words = get_sentence_words(sentence)
+    prev_event = None
     for event_number, event in enumerate(task["events"]):
         record = base.copy()
+        target_word_number = get_target_word_idx(
+            event, prev_event, sentence, sentence_words
+        )
+        record["target_word_number"] = target_word_number
+        record["target_word"] = (
+            None if target_word_number is None else sentence_words[target_word_number]
+        )
         record["event_number"] = event_number
         copy_rename(event, record, event_columns)
         yield record
+        prev_event = event
 
 
 if __name__ == "__main__":
+
+    def log_progress(number_of_written_rows):
+        if number_of_written_rows > 0:
+            print("\033[F", end="")
+        print(
+            "{:,} rows written in {}.".format(number_of_written_rows, output_file_path),
+        )
+
     header = list(
         chain(
             ["run_id", "event_number"]
@@ -75,6 +125,7 @@ if __name__ == "__main__":
             log_columns.keys(),
             trial_columns.keys(),
             event_columns.keys(),
+            ["target_word_number", "target_word"],
         )
     )
     csv_export(
@@ -85,5 +136,5 @@ if __name__ == "__main__":
         iter_typing_tasks,
         participant_registry_path=p_registry_path if IS_ANONYMOUS else None,
         run_registry_path=r_registry_path,
+        log_progress=log_progress,
     )
-    print("{} written.".format(output_file_path))
